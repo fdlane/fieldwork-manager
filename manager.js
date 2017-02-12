d977745e55a3515820d56b5733439dbabb02a5c5
Donald E Nash <donaldnash1989@gmail.com>
2017-02-11 21:54:35 -0500

    Add a single page front-end to demonstrate live data updates (#7)

--- manager.js
+++ manager.js
@@ -1,9 +1,11 @@
 var admin = require("firebase-admin");
+var express = require('express');
 const app = require('express')();
 const routes = require('./routes/routes');
 
+app.use(express.static('public'));
 app.use('/', routes);
 
-app.listen(process.env.PORT || 8081, function () {
-   console.log("Manager server started successfully");
+app.listen(process.env.PORT || 5000, function () {
+   console.log("Manager server started successfully at: 'http://localhost:5000'");
 })
