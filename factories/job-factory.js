976f22f4d68fc41f7a289973f0e0eb7c25aa14c0
Donald E Nash <donaldnash1989@gmail.com>
2017-03-30 13:59:59 -0400

    Update job-factory.js

--- factories/job-factory.js
+++ factories/job-factory.js
@@ -1,23 +1,24 @@
 var faker = require('faker');
 
 var usernames = ['maudevolk','neon.vnm','fdlane61','amberolive','zachary','donaldnash1989','mkaminski69'];
+var categories = ['outage', 'replacement', 'service'];
+var images = ['PowerLine.jpg', 'meter.jpg', 'broken-sign.jpg', 'pothole.jpg'];
 
 module.exports = function generateJobs(){
   return{
     createdBy: 'fieldwork-manager',
+    description: 'some description',
     createdDate: new Date(),
     editedBy: '',
-    editedDate: '',
+    editedDate: new Date(),
+    contactInfo: 'customer@email.com',
+    category: faker.random.arrayElement(categories),
     location: faker.address.streetAddress(),
     metaId: faker.random.number({min: 1000000000, max: 9999999999}),
     scheduleDate: faker.date.recent(),
     assignedTo: faker.random.arrayElement(usernames),
     isActive: true,
-    images: {
-      "PowerLine.jpg",
-      "meter.jpg",
-      "broken-sign.jpg",
-      "pothole.jpg",
-    },
+    status:'pending',
+    jobImage: faker.random.arrayElement(images),
   }
 };
